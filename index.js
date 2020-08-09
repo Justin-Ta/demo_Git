48d37781fa893b102a656e86d865af6ad13c1326
Quang Duy Ta <taquangduy1228@gmail.com>
2020-08-09 10:49:18 +0700

    edit_deploy

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
 console.log("master");
 
-<<<<<<< HEAD
-console.log("deployBranch");
-=======
-<<<<<<< HEAD
 console.log("testBranch");
-=======
 console.log("devBranch");
->>>>>>> dev
->>>>>>> test
