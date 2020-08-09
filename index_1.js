4e28e38e6a12f5b46f5f715f0a8f8157d43f4513
Quang Duy Ta <taquangduy1228@gmail.com>
2020-08-09 11:14:04 +0700

    merge_all

--- index_1.js
+++ index_1.js
@@ -1,9 +1,6 @@
 var f_initBranch= function {
-<<<<<<< HEAD
     console.log("Console_log_1");
     console.log("done_f1");
-=======
     console.log("console_log_f2");
     console.log("done_f2");
->>>>>>> feture2
 }
