499a8c47de3045b48e8ca64d08cc0f7016fd5c37
Quang Duy Ta <taquangduy1228@gmail.com>
2020-08-09 10:48:35 +0700

    merge_deploy_test

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 console.log("master");
 
+<<<<<<< HEAD
 console.log("deployBranch");
+=======
+<<<<<<< HEAD
+console.log("testBranch");
+=======
+console.log("devBranch");
+>>>>>>> dev
+>>>>>>> test
